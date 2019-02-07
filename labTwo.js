768a675c074b76e93223d1281e12b89acff160fa
Lee Kendall <lee.kendall@centre.edu>
2019-02-07 15:21:44 -0500

    k

--- labTwo.js
+++ labTwo.js
@@ -2,5 +2,5 @@ var snap = function()
 {
   var snapButton = document.getElementById('button');
   var vict = document.getElementById("Vict");
-  snapButton.onclick = vict.innterText(" XD :O XD :O")
+  vict.innterText(" XD :O XD :O");
 }
