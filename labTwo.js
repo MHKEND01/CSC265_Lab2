19d197017c7fa10971fc6a352c8a02d0d07bdd38
MHKEND01 <47224722+MHKEND01@users.noreply.github.com>
2019-02-08 17:52:11 -0500

    Update labTwo.js

--- labTwo.js
+++ labTwo.js
@@ -2,23 +2,28 @@ var con = function()
 {
   var before = document.createElement("img");
   before.src = "snap.jpg";
+  before.setAttribute("id", "pic");
   var resetButton = document.getElementById("resetButton")
-  resetButton.appendChild(before);
-  window.alert("hi")
+  document.body.appendChild(before);
+  document.getElementById('snapButton').innerText = 'SNAP';
+ document.getElementById('snapButton').onclick = snap;
+ document.getElementById("resetButton").disabled=false;
 }
-
-
-
-
-// var snap = function()
-// {
-//   var vict = document.getElementById("Vict");
-//   vict.innerText =" (✖╭╮✖) (☉_☉) (✖╭╮✖) (☉_☉)";
-//   document.getElementById("resetButton").disabled=false;
-// }
+var snap = function()
+{
+  var vict = document.getElementById("Vict");
+  vict.innerText =" (✖╭╮✖) (☉_☉) (✖╭╮✖) (☉_☉)";
+  document.getElementById('pic').src = 'actualSnap.jpg';
+  document.getElementById("snapButton").disabled=true;
+}
 
 var reset = function()
 {
   var vict=document.getElementById("Vict");
   vict.innerText="(◕‿◕✿) (◕‿◕✿) (◕‿◕✿) (◕‿◕✿)"
+  document.getElementById('snapButton').innerText = 'Continue';
+  document.getElementById('snapButton').onclick = con;
+  document.getElementById("snapButton").disabled=false;
+  document.getElementById("resetButton").disabled=true;
+  document.getElementById('pic').parentNode.removeChild(document.getElementById('pic'));
 }
