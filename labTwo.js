2f88f89fb9c187d5e4681aace70658461eab4fc8
Daniel Bolster <daniel.bolster@centre.edu>
2019-02-08 15:36:44 -0500

    update

--- labTwo.js
+++ labTwo.js
@@ -1,9 +1,20 @@
-var snap = function()
+var con = function()
 {
-  var vict = document.getElementById("Vict");
-  vict.innerText =" (✖╭╮✖) (☉_☉) (✖╭╮✖) (☉_☉)";
-  document.getElementById("resetButton").disabled=false;
+  removeElement("speech");
+  var before = document.createElement("img");
+  before.src = "snap.jpg";
+  resetButton.appendChild(before);
 }
+
+
+
+
+// var snap = function()
+// {
+//   var vict = document.getElementById("Vict");
+//   vict.innerText =" (✖╭╮✖) (☉_☉) (✖╭╮✖) (☉_☉)";
+//   document.getElementById("resetButton").disabled=false;
+// }
 
 var reset = function()
 {
