284b6817f2c5ab46228c7c16c04321279b7bf3a6
Daniel Bolster <daniel.bolster@centre.edu>
2019-02-08 15:06:05 -0500

    reset button

--- labTwo.js
+++ labTwo.js
@@ -2,4 +2,11 @@ var snap = function()
 {
   var vict = document.getElementById("Vict");
   vict.innerText =" (✖╭╮✖) (☉_☉) (✖╭╮✖) (☉_☉)";
+  document.getElementById("resetButton").disabled=false;
+}
+
+var reset = function()
+{
+  var vict=document.getElementById("Vict");
+  vict.innerText="(◕‿◕✿) (◕‿◕✿) (◕‿◕✿) (◕‿◕✿)"
 }
